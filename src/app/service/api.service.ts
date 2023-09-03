5ff82ccb756b38b0fa56e819bd0b4809555e279e
JuanDVilla <juandvillamontoya@gmail.com>
2023-09-03 12:41:36 -0500

    cambios api service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class ApiService {
 
   constructor(private http: HttpClient) { }
 
   public getData(fieldFilter: any, filter: any, amount: any, actualPage: any): Observable<any>{
-    const urlApi = 'http://localhost/Prueba/test_module/web/API/formulario?fieldFilter=' + fieldFilter + '&filter=' + filter + '&amount=' + amount + '&actualPage=' + actualPage
+    const urlApi = 'http://localhost/Prueba/Drupal/test_module/web/API/formulario?fieldFilter=' + fieldFilter + '&filter=' + filter + '&amount=' + amount + '&actualPage=' + actualPage
     return this.http.get<any>(urlApi);
   }
 }
